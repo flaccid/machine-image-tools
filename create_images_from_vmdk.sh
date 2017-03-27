33602d1bc6bc014c1d4b95407f5af117c1ddc183
Chris Fordham <chris@fordham-nagy.id.au>
2017-03-27 14:00:17 +1100

    Make script also work on macOS.

--- create_images_from_vmdk.sh
+++ create_images_from_vmdk.sh
@@ -14,7 +14,7 @@
 # derived template variables
 PRODUCT_NAME="$image"
 IMAGE_FILENAME="$image-disk1.vmdk"
-IMAGE_SIZE=$(stat --printf="%s" "$image-disk1.vmdk")
+IMAGE_SIZE=$(wc -c "$image-disk1.vmdk" | awk {'print $1'})
 IMAGE_CAPACITY=$(qemu-img info "$image-disk1.vmdk" | grep 'virtual size:' | tail -n1 | awk -F " " '{print $NF}')
 IMAGE_POPULATED_SIZE=0
 IMAGE_UUID=$(vboxmanage showhdinfo "$image-disk1.vmdk" | grep UUID | head -n1 | cut -d ':' -f 2 | xargs)
@@ -23,18 +23,18 @@ IMAGE_UUID=$(vboxmanage showhdinfo "$image-disk1.vmdk" | grep UUID | head -n1 | 
 cp -v "$OVF_TEMPLATE" "$image.ovf"
 
 # render the template
-sed -i "s/\$IMAGE_FILENAME/$IMAGE_FILENAME/" "$image.ovf"
-sed -i "s/\$IMAGE_SIZE/$IMAGE_SIZE/" "$image.ovf"
-sed -i "s/\$IMAGE_CAPACITY/$IMAGE_CAPACITY/" "$image.ovf"
-sed -i "s/\$IMAGE_POPULATED_SIZE/$IMAGE_POPULATED_SIZE/" "$image.ovf"
-sed -i "s/\$IMAGE_UUID/$IMAGE_UUID/" "$image.ovf"
-sed -i "s/\$PRODUCT_INFO/$PRODUCT_INFO/" "$image.ovf"
-sed -i "s/\$PRODUCT_NAME/$PRODUCT_NAME/" "$image.ovf"
-sed -i "s/\$PRODUCT_VERSION/$PRODUCT_VERSION/" "$image.ovf"
-sed -i "s/\$PRODUCT_FULL_VERSION/$PRODUCT_FULL_VERSION/" "$image.ovf"
-sed -i "s/\$VENDOR_NAME/$VENDOR_NAME/" "$image.ovf"
-sed -i "s/\$OPERATING_SYSTEM_INFO/$OPERATING_SYSTEM_INFO/" "$image.ovf"
-sed -i "s/\$OPERATING_SYSTEM_DESCRIPTION/$OPERATING_SYSTEM_DESCRIPTION/" "$image.ovf"
+sed -i -e "s/\$IMAGE_FILENAME/$IMAGE_FILENAME/" "$image.ovf"
+sed -i -e "s/\$IMAGE_SIZE/$IMAGE_SIZE/" "$image.ovf"
+sed -i -e "s/\$IMAGE_CAPACITY/$IMAGE_CAPACITY/" "$image.ovf"
+sed -i -e "s/\$IMAGE_POPULATED_SIZE/$IMAGE_POPULATED_SIZE/" "$image.ovf"
+sed -i -e "s/\$IMAGE_UUID/$IMAGE_UUID/" "$image.ovf"
+sed -i -e "s/\$PRODUCT_INFO/$PRODUCT_INFO/" "$image.ovf"
+sed -i -e "s/\$PRODUCT_NAME/$PRODUCT_NAME/" "$image.ovf"
+sed -i -e "s/\$PRODUCT_VERSION/$PRODUCT_VERSION/" "$image.ovf"
+sed -i -e "s/\$PRODUCT_FULL_VERSION/$PRODUCT_FULL_VERSION/" "$image.ovf"
+sed -i -e "s/\$VENDOR_NAME/$VENDOR_NAME/" "$image.ovf"
+sed -i -e "s/\$OPERATING_SYSTEM_INFO/$OPERATING_SYSTEM_INFO/" "$image.ovf"
+sed -i -e "s/\$OPERATING_SYSTEM_DESCRIPTION/$OPERATING_SYSTEM_DESCRIPTION/" "$image.ovf"
 
 echo "$image.ovf contents:"
 echo '--'
@@ -43,8 +43,8 @@ echo '--'
 
 # create mf
 cat <<EOF> "$image.mf"
-SHA1($image-disk1.vmdk)= $(sha1sum $image-disk1.vmdk | cut -d ' ' -f 1)
-SHA1($image.ovf)= $(sha1sum $image.ovf | cut -d ' ' -f 1)
+SHA1($image-disk1.vmdk)= $(shasum -a 1 $image-disk1.vmdk | cut -d ' ' -f 1)
+SHA1($image.ovf)= $(shasum -a 1 $image.ovf | cut -d ' ' -f 1)
 EOF
 echo "-- $image.mf -- "
 cat "$image.mf"
