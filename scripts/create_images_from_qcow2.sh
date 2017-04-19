500ce46ea7bbc95008e22e4ce695798b929ae44c
Chris Fordham <chris@fordham-nagy.id.au>
2017-04-19 10:43:24 +1000

    Fixes/enhancements to support FILENAME_PREFIX override.

--- scripts/create_images_from_qcow2.sh
+++ scripts/create_images_from_qcow2.sh
@@ -12,6 +12,19 @@
 
 : "${IMAGE_URL:=http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2}"
 : "${IMAGE_SHA256SUM:=0cfd71bfbb4dba3097999dcbe1e611d6c3407f1b30936e9a6e437f320dfb7be9}"
+: "${FILENAME_PREFIX:=$(basename "$IMAGE_URL")}"
+
+# user specified template variables
+: "${OVF_TEMPLATE:=../templates/basic_template.ovf.xml}"
+: "${PRODUCT_INFO:=A Linux Host}"
+: "${PRODUCT_VERSION:=latest}"
+: "${PRODUCT_FULL_VERSION:=latest}"
+: "${VENDOR_NAME:=ACME Inc.}"
+: "${OPERATING_SYSTEM_INFO:=Linux}"
+: "${OPERATING_SYSTEM_DESCRIPTION:=Linux}"
+: "${OPERATING_SYSTEM_ID:=101}"
+: "${OPERATING_SYSTEM_VERSION:=16.04}"
+: "${OPERATING_SYSTEM_TYPE:=Linux 64-Bit}"
 
 # ensure the contrib/ is in path
 export PATH="$PATH:$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../contrib"
@@ -34,99 +47,84 @@ verify_image()
 	fi
 }
 
-image=$(basename "$IMAGE_URL")
-ext="${image##*.}"
+image_filename="${IMAGE_URL##*/}"
+# image_ext="${image_filename##*.}"
 
-echo "create images from $image"
+echo "create images from $image_filename"
 
 # verify checksum
-if [ ! -e "$image" ]; then
+if [ ! -e "$image_filename" ]; then
 	echo "--> fetching $IMAGE_URL"
-	curl -LSs "$IMAGE_URL" > "$image"
-	! verify_image "$image" && exit 1
+	curl -LSs "$IMAGE_URL" > "$image_filename"
+	! verify_image "$image_filename" && exit 1
 else
 	echo 'file already downloaded.'
-	! verify_image "$image" && exit 1
+	! verify_image "$image_filename" && exit 1
 fi
 
+# TODO
 # unarchive if needed etc.
 
-# strip out extension(s)
-image="${image%.*}"
-
 # convert qcow2 to raw
-echo "converting $image.qcow2 to raw..."
-if [ ! -e "$FILENAME_PREFIX" ]; then
-	qemu-img convert "$image.qcow2" "$FILENAME_PREFIX.img"
-else
-	qemu-img convert "$image.qcow2" "$image.img"
-fi
+echo "converting $image_filename to raw..."
+qemu-img convert "$image_filename" "$FILENAME_PREFIX.img"
 
 # convert qcow2 to vmdk
 # 		of note:
 # 			qemu-img convert -f qcow2 -O vmdk "$image.qcow2" "$image-disk1.vmdk"
 # 			python2.7 /usr/lib/python2.7/dist-packages/VMDKstream.py "$image.img" "$image-disk1.vmdk"
-echo "converting $image.qcow2 to vmdk..."
-if [ ! -e "$FILENAME_PREFIX" ]; then
-	img-convert "$image.qcow2" vmdk-stream "$FILENAME_PREFIX-disk1.vmdk"
-	qemu-img info "$FILENAME_PREFIX-disk1.vmdk"
-else
-	img-convert "$image.qcow2" vmdk-stream "$image-disk1.vmdk"
-	qemu-img info "$image-disk1.vmdk"
-fi
+echo "converting $image_filename to vmdk..."
+img-convert "$image_filename" vmdk-stream "$FILENAME_PREFIX-disk1.vmdk"
+qemu-img info "$FILENAME_PREFIX-disk1.vmdk"
 
 # vboxmanage showhdinfo "$image-disk1.vmdk"
 
 # copy the ovf template
-cp -v "$OVF_TEMPLATE" "$image.ovf"
+cp -v "$OVF_TEMPLATE" "$FILENAME_PREFIX.ovf"
 
-IMAGE_FILENAME="$image-disk1.vmdk"
-IMAGE_SIZE=$(stat --printf="%s" "$image-disk1.vmdk")
-IMAGE_CAPACITY=$(qemu-img info "$image-disk1.vmdk" | grep 'virtual size:' | tail -n1 | awk -F " " '{print $NF}')
+IMAGE_FILENAME="$FILENAME_PREFIX-disk1.vmdk"
+IMAGE_SIZE=$(stat --printf="%s" "$FILENAME_PREFIX-disk1.vmdk")
+IMAGE_CAPACITY=$(qemu-img info "$FILENAME_PREFIX-disk1.vmdk" | grep 'virtual size:' | tail -n1 | awk -F " " '{print $NF}')
+IMAGE_UUID=$(vboxmanage showhdinfo "$FILENAME_PREFIX-disk1.vmdk" | grep UUID | head -n1 | cut -d ':' -f 2 | xargs)
+PRODUCT_NAME="$FILENAME_PREFIX"
+
+# derived template variables
 IMAGE_POPULATED_SIZE=0
-IMAGE_UUID=$(vboxmanage showhdinfo "$image-disk1.vmdk" | grep UUID | head -n1 | cut -d ':' -f 2 | xargs)
-PRODUCT_INFO='A base RHEL host.'
-PRODUCT_NAME="$image"
-PRODUCT_VERSION='7.2'
-PRODUCT_FULL_VERSION='7.2-20151102.0'
-VENDOR_NAME='Red Hat Inc.'
-OPERATING_SYSTEM_INFO='Linux'
-OPERATING_SYSTEM_DESCRIPTION='Red Hat Enterprise Linux Guest Machine'
 
 # render the template
-sed -i -e "s/\$IMAGE_FILENAME/$IMAGE_FILENAME/" "$image.ovf"
-sed -i -e "s/\$IMAGE_SIZE/$IMAGE_SIZE/" "$image.ovf"
-sed -i -e "s/\$IMAGE_CAPACITY/$IMAGE_CAPACITY/" "$image.ovf"
-sed -i -e "s/\$IMAGE_POPULATED_SIZE/$IMAGE_POPULATED_SIZE/" "$image.ovf"
-sed -i -e "s/\$IMAGE_UUID/$IMAGE_UUID/" "$image.ovf"
-sed -i -e "s/\$PRODUCT_INFO/$PRODUCT_INFO/" "$image.ovf"
-sed -i -e "s/\$PRODUCT_NAME/$PRODUCT_NAME/" "$image.ovf"
-sed -i -e "s/\$PRODUCT_VERSION/$PRODUCT_VERSION/" "$image.ovf"
-sed -i -e "s/\$PRODUCT_FULL_VERSION/$PRODUCT_FULL_VERSION/" "$image.ovf"
-sed -i -e "s/\$VENDOR_NAME/$VENDOR_NAME/" "$image.ovf"
-sed -i -e "s/\$OPERATING_SYSTEM_INFO/$OPERATING_SYSTEM_INFO/" "$image.ovf"
-sed -i -e "s/\$OPERATING_SYSTEM_DESCRIPTION/$OPERATING_SYSTEM_DESCRIPTION/" "$image.ovf"
-sed -i -e "s/\$OPERATING_SYSTEM_ID/$OPERATING_SYSTEM_ID/" "$image.ovf"
-sed -i -e "s/\$OPERATING_SYSTEM_VERSION/$OPERATING_SYSTEM_VERSION/" "$image.ovf"
-sed -i -e "s/\$OPERATING_SYSTEM_TYPE/$OPERATING_SYSTEM_TYPE/" "$image.ovf"
+sed -i -e "s/\$IMAGE_FILENAME/$IMAGE_FILENAME/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$IMAGE_SIZE/$IMAGE_SIZE/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$IMAGE_CAPACITY/$IMAGE_CAPACITY/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$IMAGE_POPULATED_SIZE/$IMAGE_POPULATED_SIZE/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$IMAGE_UUID/$IMAGE_UUID/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$PRODUCT_INFO/$PRODUCT_INFO/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$PRODUCT_NAME/$PRODUCT_NAME/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$PRODUCT_VERSION/$PRODUCT_VERSION/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$PRODUCT_FULL_VERSION/$PRODUCT_FULL_VERSION/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$VENDOR_NAME/$VENDOR_NAME/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$OPERATING_SYSTEM_INFO/$OPERATING_SYSTEM_INFO/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s%\$OPERATING_SYSTEM_DESCRIPTION%$OPERATING_SYSTEM_DESCRIPTION%" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$OPERATING_SYSTEM_ID/$OPERATING_SYSTEM_ID/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$OPERATING_SYSTEM_VERSION/$OPERATING_SYSTEM_VERSION/" "$FILENAME_PREFIX.ovf"
+sed -i -e "s/\$OPERATING_SYSTEM_TYPE/$OPERATING_SYSTEM_TYPE/" "$FILENAME_PREFIX.ovf"
 
-echo "$image.ovf contents:"
+echo "$FILENAME_PREFIX.ovf contents:"
 echo '--'
-cat "$image.ovf"
+cat "$FILENAME_PREFIX.ovf"
 echo '--'
 
 # create mf
-cat <<EOF> "$image.mf"
-SHA1($image-disk1.vmdk)= $(sha1sum $image-disk1.vmdk | cut -d ' ' -f 1)
-SHA1($image.ovf)= $(sha1sum $image.ovf | cut -d ' ' -f 1)
+cat <<EOF> "$FILENAME_PREFIX.mf"
+SHA1($FILENAME_PREFIX-disk1.vmdk)= $(sha1sum $FILENAME_PREFIX-disk1.vmdk | cut -d ' ' -f 1)
+SHA1($FILENAME_PREFIX.ovf)= $(sha1sum $FILENAME_PREFIX.ovf | cut -d ' ' -f 1)
 EOF
-echo "-- $image.mf -- "
-cat "$image.mf"
+echo "-- $FILENAME_PREFIX.mf -- "
+cat "$FILENAME_PREFIX.mf"
 echo '----'
 
 # create vmware ova
 echo 'Creating ova...'
-tar cvf "$image.ova" "$image.ovf"
-tar uvf "$image.ova" "$image.mf" "$image-disk1.vmdk"
+tar cvf "$FILENAME_PREFIX.ova" "$FILENAME_PREFIX.ovf"
+tar uvf "$FILENAME_PREFIX.ova" "$FILENAME_PREFIX.mf" "$FILENAME_PREFIX-disk1.vmdk"
 
 echo 'Done.'
